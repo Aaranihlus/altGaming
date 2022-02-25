d441f0876c53f224f564947d255bf3d4f54ac5a7
Nathan <nathanl38@hotmail.com>
2022-02-25 13:48:37 +0000

    Update post template

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -11,20 +11,24 @@
     <h6 style="text-align: center;">{{ \Carbon\Carbon::parse( $post->created_at )->diffForHumans() }}</h6>
 
     @if($post->type == "podcast")
-      <p>
-        {{ $post->description }}
-      </p>
+      <div class="flex-y" style="align-items: center;">
+        <p style="text-align: center;">
+          {{ $post->description }}
+        </p>
+        @if ( !empty($post->audio_file) )
+        <div>
+          <audio controls src="{{ asset("storage/" . $post->audio_file) }}">Your browser does not support the <code>audio</code> element.</audio>
+        </div>
+        @endif
+
+        @if ( !empty($post->youtube_link) )
+          <iframe width="420" height="315" src="{{$post->youtube_link}}"></iframe>
+        @endif
+      </div>
     @endif
 
     @if($post->type == "blog")
       <hr>
-    @endif
-
-
-    @if ( !empty($post->audio_file) )
-    <div>
-      <audio controls src="{{ asset("storage/" . $post->audio_file) }}">Your browser does not support the <code>audio</code> element.</audio>
-    </div>
     @endif
 
     <div>
