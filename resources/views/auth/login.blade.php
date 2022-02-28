12a618f347f31a15726ba8ee51edde6e84136810
Nathan <nathanl38@hotmail.com>
2022-02-28 13:46:16 +0000

    Potential layout fix for register and login pages on mobile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,42 +4,48 @@
 
   <div class="flex-x" style="height: 80vh; align-items: center;">
 
-  <div class="container bg-alt-yellow p-4 extra-rounded" style="width: 25%;">
+    <div class="container">
 
-    <h1 class="text-xl font-bold uppercase">Welcome back</h1>
+      <div class="row" style="justify-content: center">
 
-    <hr>
+        <div class="offset-lg-2 offset-xl-2 offset-sm-2 offset-xs-2 offset-md-2"></div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+        <div class="col-lg-8 col-xl-8 col-sm-8 col-xs-8 col-md-8 bg-alt-yellow p-4 extra-rounded">
+          <h1 class="text-xl font-bold uppercase">Welcome back</h1>
 
-    <!-- Validation Errors -->
-    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+          <hr>
 
-    <form method="POST" action="/login">
-      @csrf
+          <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-      <div class="mb-3">
-        <label for="username" class="form-label">Username</label>
-        <input type="text" class="form-control" id="username" name="username">
-      </div>
+          <form method="POST" action="/login">
+            @csrf
 
-      <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="text" class="form-control" id="password" name="password">
-      </div>
+            <div class="mb-3">
+              <label for="username" class="form-label">Username</label>
+              <input type="text" class="form-control" id="username" name="username">
+            </div>
+
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="text" class="form-control" id="password" name="password">
+            </div>
 
 
-      <div style="display: flex; align-items: center; justify-content: space-between;">
-        @if (Route::has('password.request'))
-            <!--<a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+            <div style="display: flex; align-items: center; justify-content: space-between;">
+              @if (Route::has('password.request'))
+                <!--<a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                 {{ __('Forgot your password?') }}
-            </a>-->
-        @endif
+              </a>-->
+            @endif
 
-        <button type="submit" class="btn btn-warning">Log In</button>
+            <button type="submit" class="btn btn-warning">Log In</button>
+          </div>
+        </form>
       </div>
-    </form>
+
+      <div class="offset-lg-2 offset-xl-2 offset-sm-2 offset-xs-2 offset-md-2"></div>
+
+    </div>
 
   </div>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,49 +4,60 @@
 
   <div class="flex-x" style="height: 80vh; align-items: center;">
 
-  <div class="container bg-alt-yellow p-4 extra-rounded" style="width: 25%;">
+    <div class="container">
 
-    <h1 class="text-xl font-bold uppercase">Register</h1>
-    <span>You will be able to set up your profile after registration</span>
+      <div class="row" style="justify-content: center">
 
-    <hr>
+        <div class="offset-lg-2 offset-xl-2 offset-sm-2 offset-xs-2 offset-md-2"></div>
 
-    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <div class="col-lg-8 col-xl-8 col-sm-8 col-xs-8 col-md-8 bg-alt-yellow p-4 extra-rounded">
 
-    <form method="POST" action="/register">
-      @csrf
+          <h1 class="text-xl font-bold uppercase">Register</h1>
+          <span>You will be able to set up your profile after registration</span>
 
-      <div class="mb-3">
-        <label for="username" class="form-label">Username</label>
-        <input type="text" class="form-control" id="username" name="username">
+          <hr>
+
+          <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+          <form method="POST" action="/register">
+            @csrf
+
+            <div class="mb-3">
+              <label for="username" class="form-label">Username</label>
+              <input type="text" class="form-control" id="username" name="username">
+            </div>
+
+            <div class="mb-3">
+              <label for="email" class="form-label">Email Address</label>
+              <input type="text" class="form-control" id="email" name="email">
+            </div>
+
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="text" class="form-control" id="password" name="password">
+            </div>
+
+            <div class="mb-3">
+              <label for="password_confirmation" class="form-label">Confirm Password</label>
+              <input type="text" class="form-control" id="password_confirmation" name="password_confirmation">
+            </div>
+
+            <div style="display: flex; align-items: center; justify-content: space-between;">
+              <a class="underline text-sm text-alt-yellow hover:text-white" href="/login">
+                {{ __('Already registered?') }}
+              </a>
+
+              <button type="submit" class="btn btn-warning mx-1">Sign Up</button>
+            </div>
+          </form>
+
+        </div>
+
       </div>
 
-      <div class="mb-3">
-        <label for="email" class="form-label">Email Address</label>
-        <input type="text" class="form-control" id="email" name="email">
-      </div>
-
-      <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="text" class="form-control" id="password" name="password">
-      </div>
-
-      <div class="mb-3">
-        <label for="password_confirmation" class="form-label">Confirm Password</label>
-        <input type="text" class="form-control" id="password_confirmation" name="password_confirmation">
-      </div>
-
-      <div style="display: flex; align-items: center; justify-content: space-between;">
-        <a class="underline text-sm text-alt-yellow hover:text-white" href="/login">
-          {{ __('Already registered?') }}
-        </a>
-
-        <button type="submit" class="btn btn-warning mx-1">Sign Up</button>
-      </div>
-    </form>
+    </div>
 
   </div>
 
-  </div>
 
 @endsection
