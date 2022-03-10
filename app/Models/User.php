b46051f73ebbd1729ea13e0ce88872ffdba205e3
Nathan <nathanl38@hotmail.com>
2022-03-10 20:27:03 +0000

    uncomment model relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,7 +62,7 @@ class User extends Authenticatable
       return $this->belongsToMany(Role::class);
     }
 
-    /*public function titles() {
+    public function titles() {
       return $this->belongsToMany(Title::class);
     }
 
@@ -84,6 +84,6 @@ class User extends Authenticatable
 
     public function orders() {
       return $this->hasMany(Order::class);
-    }*/
+    }
 
 }
