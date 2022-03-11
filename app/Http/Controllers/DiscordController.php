126bc2cfc9d8ab882faa1d57125f25eb3bbb32b7
Nathan <nathanl38@hotmail.com>
2022-03-11 21:59:45 +0000

    actually store the slug with the initial request now

--- app/Http/Controllers/DiscordController.php
+++ app/Http/Controllers/DiscordController.php
@@ -43,8 +43,6 @@ class DiscordController extends Controller {
      */
     public function handle(Request $request) {
 
-      dd($request);
-
         // Checking if the authorization code is present in the request.
         if ($request->missing('code')) {
             if (env('APP_DEBUG')) {
