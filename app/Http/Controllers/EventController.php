15114bfcfe99bed998a573942c18fe9bb3800ab6
Nathan <nathanl38@hotmail.com>
2022-03-12 21:24:09 +0000

    try use arrow notation for discord role

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -112,7 +112,7 @@ class EventController extends Controller {
           'event_id' => $event->id,
           'visible' => 1,
           'achievement_id' => null,
-          'discord_role_id' => $discordRole['id']
+          'discord_role_id' => $discordRole->id
       ]);
 
       $standardTicketImage = ItemImage::create([
