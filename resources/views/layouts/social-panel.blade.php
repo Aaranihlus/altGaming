3ed1d9b9342b4d24e1fdf79dcf0580add22646c0
Nathan <nathanl38@hotmail.com>
2022-03-23 21:56:20 +0000

    fix facebook link

--- resources/views/layouts/social-panel.blade.php
+++ resources/views/layouts/social-panel.blade.php
@@ -1,6 +1,6 @@
 <div style="position: fixed; background: #ffc107; right: 10px; bottom: 10px; font-size: 2.2rem;" class="p-1 flex-x extra-rounded d-none d-lg-flex">
   <a target="_blank" href="https://discord.gg/jvgX8r7" class="mx-2"><i class="fab fa-discord" style="color: #1d1800;"></i></a>
-  <a target="_blank" href="https://https://www.facebook.com/altlanuk" class="mx-2"><i class="fab fa-facebook-f" style="color: #1d1800;"></i></a>
+  <a target="_blank" href="https://facebook.com/altlanuk" class="mx-2"><i class="fab fa-facebook-f" style="color: #1d1800;"></i></a>
   <a target="_blank" href="#" class="mx-2"><i class="fab fa-instagram-square" style="color: #1d1800;"></i></a>
   <a target="_blank" href="https://twitter.com/altgaminguk" class="mx-2"><i class="fab fa-twitter" style="color: #1d1800;"></i></a>
   <a target="_blank" href="#" class="mx-2"><i class="fab fa-youtube" style="color: #1d1800;"></i></a>
