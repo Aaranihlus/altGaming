03199bf103d6ed4a5ff4aee62cf5872e8f1808f1
Nathan <nathanl38@hotmail.com>
2022-03-23 21:55:13 +0000

    social links open in new tab

--- resources/views/layouts/social-panel.blade.php
+++ resources/views/layouts/social-panel.blade.php
@@ -1,7 +1,7 @@
 <div style="position: fixed; background: #ffc107; right: 10px; bottom: 10px; font-size: 2.2rem;" class="p-1 flex-x extra-rounded d-none d-lg-flex">
-  <a href="https://discord.gg/jvgX8r7" class="mx-2"><i class="fab fa-discord" style="color: #1d1800;"></i></a>
-  <a href="https://https://www.facebook.com/altlanuk" class="mx-2"><i class="fab fa-facebook-f" style="color: #1d1800;"></i></a>
-  <a href="#" class="mx-2"><i class="fab fa-instagram-square" style="color: #1d1800;"></i></a>
-  <a href="https://twitter.com/altgaminguk" class="mx-2"><i class="fab fa-twitter" style="color: #1d1800;"></i></a>
-  <a href="#" class="mx-2"><i class="fab fa-youtube" style="color: #1d1800;"></i></a>
+  <a target="_blank" href="https://discord.gg/jvgX8r7" class="mx-2"><i class="fab fa-discord" style="color: #1d1800;"></i></a>
+  <a target="_blank" href="https://https://www.facebook.com/altlanuk" class="mx-2"><i class="fab fa-facebook-f" style="color: #1d1800;"></i></a>
+  <a target="_blank" href="#" class="mx-2"><i class="fab fa-instagram-square" style="color: #1d1800;"></i></a>
+  <a target="_blank" href="https://twitter.com/altgaminguk" class="mx-2"><i class="fab fa-twitter" style="color: #1d1800;"></i></a>
+  <a target="_blank" href="#" class="mx-2"><i class="fab fa-youtube" style="color: #1d1800;"></i></a>
 </div>
