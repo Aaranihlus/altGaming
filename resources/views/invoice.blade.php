52cb571053422543efa97d9e65021a20559007fa
Nathan <nathanl38@hotmail.com>
2022-03-25 15:35:15 +0000

    attempt 3

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -3,9 +3,9 @@
     <div class="row">
 
       <div class="col-lg-8">
-        <h1>Order #{{ $data['paypal_id'] }}</h1>
+        <h1>Order #{{ $paypal_id }}</h1>
 
-        @foreach( $data['order_items'] as $item )
+        @foreach( $order_items as $item )
           <div class="bg-alt-yellow p-2 extra-rounded my-2" style="width: 100%; display: flex; align-content: center; align-items: center; justify-content: space-between;">
             <div style="display: flex; align-items: center;">
               <h5 class="mx-3">{{ $item['name'] }}</h5>
@@ -17,7 +17,7 @@
           </div>
         @endforeach
 
-        <h1>Total: £{{ $data['order_total'] }}</h1>
+        <h1>Total: £{{ $order_total }}</h1>
 
       </div>
 
