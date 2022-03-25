5ddb6e0745cc180f0bce5983ea9967435adda960
Nathan <nathanl38@hotmail.com>
2022-03-25 16:00:16 +0000

    update template

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -5,21 +5,21 @@
   <div class="row">
 
     <div class="col-lg-8">
-      <h1>Order #{{ $order->paypal_id }}</h1>
+      <h1>Order #{{ $paypal_id }}</h1>
 
-      @foreach( $order->items as $k => $v )
+      @foreach( $order_items as $item )
         <div class="bg-alt-yellow p-2 extra-rounded my-2" style="width: 100%; display: flex; align-content: center; align-items: center; justify-content: space-between;">
           <div style="display: flex; align-items: center;">
-            <h5 class="mx-3">{{ $v->item->name }}</h5>
+            <h5 class="mx-3">{{ $item['name'] }}</h5>
           </div>
           <div style="display: flex; align-items: center;">
-            <h5 class="mx-4 my-0">Quantity: {{ $v->quantity }}</h5>
-            <h5 class="mx-4 my-0">£ {{ $v->item->price }}</h5>
+            <h5 class="mx-4 my-0">Quantity: {{ $item['quantity'] }}</h5>
+            <h5 class="mx-4 my-0">£ {{ $item['price'] }}</h5>
           </div>
         </div>
       @endforeach
 
-      <h1>Total: £{{ $order->amount }}</h1>
+      <h1>Total: £{{ $order_total }}</h1>
 
     </div>
 
