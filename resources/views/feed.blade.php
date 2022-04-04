acf2e5980f72fac38aea5cb1af3030ec9a8da59a
Nathan <nathanl38@hotmail.com>
2022-04-04 23:37:53 +0100

    rss feed testing 2

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,20 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">
     <channel>
-        <title><![CDATA[ altGaming Podcast ]]></title>
-        <link><![CDATA[ {{ url('/') }}/feed ]]></link>
-        <description><![CDATA[ altGaming | Videos, words and thoughts about gaming ]]></description>
-        <language>en</language>
+        <title>altGaming Podcast</title>
+        <link>{{ url('/') . "/podcast" }}</link>
+        <description>altGaming | Videos, words and thoughts about gaming</description>
+        <language>en-us</language>
         <pubDate>{{ now() }}</pubDate>
-
         @foreach($podcasts as $podcast)
             <item>
-                <title><![CDATA[{{ $podcast->title }}]]></title>
-                <link>{{ url('/') . "/podcast/" . $podcast->slug }}</link>
-                <description><![CDATA[{{ $podcast->description }}]]></description>
+                <title>{{ $podcast->title }}</title>
+                <enclosure url="{{ asset("storage/" . $podcast->audio_file) }}" type="audio/mp3" length="{{ \Storage::size($podcast->audio_file) }}"/>
+                <description>{{ $podcast->description }}</description>
                 <category>Gaming</category>
-                <author><![CDATA[{{ $podcast->user->username  }}]]></author>
-                <guid>{{ $podcast->id }}</guid>
+                <author>{{ $podcast->user->username }}</author>
                 <pubDate>{{ $podcast->created_at->toRssString() }}</pubDate>
             </item>
         @endforeach
