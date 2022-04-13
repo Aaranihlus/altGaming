94b239220a5e58fec4d1e2c4780d17deeffba52d
Nathan <nathanl38@hotmail.com>
2022-04-13 23:56:16 +0100

    temp fix for item image scaling

--- resources/views/components/item-template.blade.php
+++ resources/views/components/item-template.blade.php
@@ -1,9 +1,7 @@
 <div class="col-3 mb-3">
-  <div class="bg-alt-yellow extra-rounded p-2 flex-y" style="height: 475px;">
+  <div class="bg-alt-yellow extra-rounded p-2 flex-y" style="height: 375px;">
 
-    <div class="col" style="flex-basis:30%;">
-      <img src='{{ asset("storage/" . $item->images[0]->path) }}' alt="Thumbnail" class="img-fluid rounded" style="width: 100%;">
-    </div>
+    <div class="col extra-rounded" style="flex-basis:50% background-attachment: unset; background-position: center; background-repeat: no-repeat; background-size: cover; background-image: url({{ asset('storage/' . $item->images[0]->path) }});"></div>
 
     <div class="col d-flex flex-y" style="justify-content: space-between;">
 
